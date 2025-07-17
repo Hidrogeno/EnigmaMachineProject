e5e55ea43fe74196a7ca594d0d4d92469bd2d1f9
Hidrogeno <hidrogenoytiempo@gmail.com>
2025-07-17 13:05:36 -0600

    Update ui.py

--- Python/ui.py
+++ Python/ui.py
@@ -3,10 +3,10 @@ def welcome_message():
     os.system("clear")
     print("Welcome to the Enigma Machine Emulator!")
 def set_shift():
-    shiftA = int(input("Give me the initial shift for rotor I :"))
-    shiftB = int(input("Give me the initial shift for rotor II :"))
-    shiftC = int(input("Give me the initial shift for rotor III :"))
-    return (shiftA,shiftB,shiftC)
+    shift_a = int(input("Give me the initial shift for rotor I :"))
+    shift_b = int(input("Give me the initial shift for rotor II :"))
+    shift_c = int(input("Give me the initial shift for rotor III :"))
+    return (shift_a,shift_b,shift_c)
 def setup():
     welcome_message()
     shift = set_shift()
